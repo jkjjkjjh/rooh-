e49ab81035cc8c1ea1941bef76501aa99f89f54b
MAHI-YT <scarystories576@gmail.com>
2026-02-18 20:55:26 +0500

    Update botstatus.js

--- plugins/botstatus.js
+++ plugins/botstatus.js
@@ -2,7 +2,7 @@ const { cmd, commands } = require('../command');
 const axios = require('axios');
 
 // Your Vercel API base URL
-const API_BASE_URL = 'https://erfanmini.vercel.app/api'; // Added /api prefix
+const API_BASE_URL = 'https://erfan-md.vercel.app/api'; // Added /api prefix
 
 // Function to get status emoji based on count
 function getCountStatus(count) {
